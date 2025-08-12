45af473d0418e29516b90f7e70a4867e9ba4e1d6
dani23 <giacche.dani@gmail.com>
2025-08-12 19:14:18 +0200

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use std::{fs, path};
+use ini::ini;
+
+fn ReadConfigFile(ConfigPath:&str) {
+    
+    let ConfigFile = ini!(ConfigFile);
+
+}
+
+fn ReadDirectory(path:&str) {
+    
+}
+
+fn main() {
+    println!("Hello, world!");
+}
